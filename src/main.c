885137a4233ba417bca04f8329e2874bd461f4bb
Kitty <ethelay1grin@gmail.com>
2025-05-06 00:13:05 +0300

    Current changes

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define UP 1
-#define SUCCESS 0
-#define ERR_USED 1
+#define VERT 1
+#define HOR 2
+
+// Коды ошибок
+enum {
+    SUCCESS 	   = 0,
+    ERR_ALLOCATION = 1,
+    ERR_NO_SPACE   = 2,
+    ERR_OCC_CELL   = 3
+};
 
 typedef struct {
     unsigned char length;
@@ -17,26 +25,40 @@ typedef struct {
     unsigned char checked;
 } Cell;
 
-int init_field(Cell  field[10][10]);
+void init_field(Cell  field[10][10]);
+
 int place_ship(Cell  field[10][10],
 	       Ship* ship, 
 	       Cell* def_cell,
 	       int   direction);
-int filling_cells_up(Cell  field[10][10],
-		     Ship* def_ship,
-		     Cell* def_cell);
+
+int is_free_space(Cell  field[10][10],
+		  Ship* def_ship,
+		  Cell* def_cell,
+		  int   direction);
+
+int filling_cells_vert(Cell  field[10][10],
+		       Ship* def_ship,
+		       Cell* def_cell);
+
+int filling_cells_hor(Cell  field[10][10],
+		       Ship* def_ship,
+		       Cell* def_cell);
+
+void log_err(int flag_error);
 
 int main() {
     Cell field[10][10];
     init_field(field);
 
     Ship* cruiser   = malloc(sizeof(Ship));
+
     cruiser->length = 4;
     cruiser->hp     = 4;
     place_ship(field,
 	       cruiser,
-	       &field[9][0],
-	       UP);
+	       &field[9][7],
+	       HOR);
     for(int i = 0; i < 10; i++) {
 	for(int j = 0; j < 10; j++) {
 	    if(field[i][j].ship != NULL)
@@ -49,19 +71,14 @@ int main() {
     return 0;
 }
 
-int init_field(Cell field[10][10]) {
-    int flag_error = SUCCESS;
-
-    for(unsigned char init_row = 0; init_row < 10; init_row++) {
+void init_field(Cell field[10][10]) {
+    for(unsigned char init_row = 0; init_row < 10; init_row++)
 	for(unsigned char init_col = 0; init_col < 10; init_col++) {
 	    field[init_row][init_col].row     = init_row;
 	    field[init_row][init_col].col     = init_col;
 	    field[init_row][init_col].checked = 0;
 	    field[init_row][init_col].ship    = NULL;
 	}
-    }
-
-    return flag_error;
 }
 
 int place_ship(Cell  field[10][10],
@@ -70,31 +87,140 @@ int place_ship(Cell  field[10][10],
 	       int   direction) {
     int flag_error = SUCCESS;
     
-    if(def_cell->ship)
-	flag_error = ERR_USED;
-    else {
-	switch(direction) {
-	    case UP:
-		filling_cells_up(field, ship, def_cell);
-		break;
-	    default:
-		break;
-	}
+    switch(direction) {
+	case VERT:
+	    flag_error = filling_cells_vert(field, 
+			    	 	    ship, 
+					    def_cell);
+	    log_err(flag_error);
+	    break;
+	case HOR:
+	    flag_error = filling_cells_hor(field, 
+			      		   ship, 
+			      	 	   def_cell);
+	    log_err(flag_error);
+	    break;
+	default:
+	    break;
     }
 }
 
-int filling_cells_up(Cell  field[10][10],
-		     Ship* def_ship,
-		     Cell* def_cell) {
+int is_free_space(Cell  field[10][10],
+		  Ship* def_ship,
+		  Cell* def_cell,
+		  int   direction) {
     int flag_error = SUCCESS;
 
     unsigned char count_len = def_ship->length;
-    for(int row  = def_cell->row;
-	    row >= 0 && count_len > 0;
-	    row--) {
-	field[row][def_cell->col].ship = def_ship;
-    	count_len--;
+    
+    if(direction == VERT) {
+    	if(count_len <= 
+           def_cell->row + 1) {
+	    for(int row = def_cell->row; 
+		count_len > 0;
+		row--) {
+	        if(field[row][def_cell->col].ship)
+	    	    flag_error = ERR_OCC_CELL;
+	    	
+		count_len--;
+	    }
+	} else
+	    flag_error = ERR_NO_SPACE;
+    }
+    if(direction == HOR) {
+	if(count_len <= 
+       	   10 - def_cell->col) {
+	    for(int col = def_cell->col; 
+		count_len > 0;
+		col++) {
+	    	if(field[def_cell->row][col].ship)
+	    	    flag_error = ERR_OCC_CELL;
+		
+		count_len--;
+	    }
+	} else
+	    flag_error = ERR_NO_SPACE;
     }
 
     return flag_error;
+}
+
+int filling_cells_vert(Cell  field[10][10],
+		       Ship* def_ship,
+		       Cell* def_cell) {
+    int flag_error = SUCCESS;
+
+    
+    unsigned char count_len = def_ship->length;
+    
+    flag_error = is_free_space(field,
+		      	       def_ship,
+		      	       def_cell,
+		      	       VERT);
+    for(int row = def_cell->row; 
+	    count_len > 0 && !flag_error; 
+            row--) {
+	field[row][def_cell->col].ship = def_ship;
+	count_len--;
+    } 
+
+    return flag_error;
+}
+
+int filling_cells_hor(Cell  field[10][10],
+		      Ship* def_ship,
+		      Cell* def_cell) {
+    int flag_error = SUCCESS;
+
+    unsigned char count_len = def_ship->length;
+    
+    flag_error = is_free_space(field,
+		      	       def_ship,
+		      	       def_cell,
+		      	       HOR);
+    for(int col = def_cell->col; 
+	    count_len > 0 && !flag_error;
+	    col++) {
+	field[def_cell->row][col].ship = def_ship;
+	count_len--;
+    } 
+
+    return flag_error;
+}
+
+void log_err(int flag_error) {
+    FILE* log_file = fopen("../log_file.txt", "a");
+    
+    
+    time_t current_time = time(NULL);
+    struct tm *time_info;
+    char   time_str[100];
+    time_info = localtime(&current_time);
+    strftime(time_str, 
+	     sizeof(time_str), 
+	     "%Y-%m-%d %H:%M:%S", time_info);
+
+    if(!log_file) {
+	flag_error = ERR_ALLOCATION;
+    	printf("Ошибка открытия файла! - %s\n", time_str);
+    } else {
+    	switch(flag_error) {
+	    case ERR_ALLOCATION:
+	    	fprintf(log_file, 
+		        "Ошибка выделения памяти! - %s\n",  time_str);
+	    	break;
+	    case ERR_NO_SPACE:
+	    	fprintf(log_file, 
+		        "Выход за пределы массива! - %s\n", time_str);
+	    	break;
+	    case ERR_OCC_CELL:
+	    	fprintf(log_file, 
+		        "Клетка занята! - %s\n", 	    time_str);
+	    	break;
+	    default:
+	    	break;
+    	}
+    }
+
+    fclose(log_file);
 }
